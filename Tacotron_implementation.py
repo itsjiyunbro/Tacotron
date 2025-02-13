88b1ae35588157e6ad07d80e2306567cb05e255e
Jiyun Baik <bjy0609@yonsei.ac.kr>
2025-02-13 21:09:32 +0900

    Update Tacotron_implementation.py
    Trivial Revision

--- Tacotron_implementation.py
+++ Tacotron_implementation.py
@@ -16,35 +16,16 @@ waveglow.eval()
 
 
 
-# 파이썬 환경에서 TTS 재생하기
+# 파이썬 환경 재생
 from IPython.display import Audio
-
-text = """
-Smmoth like butter Like a criminal undercover
-Gon' pop like trouble Breakin' into your heart like that Cool shade stunner
-Yeah I owe it all to my mother Hot like summer.
-"""
-
-utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tts_utils')
-sequences, lengths = utils.prepare_input_sequence([text])
-
-with torch.no_grad():
-  mel, _, _ = tacotron2.infer(sequences, lengths)
-  audio = waveglow.infer(mel)
-audio_numpy = audio[0].data.cpu().numpy()
-rate = 22050
-
-Audio(audio_numpy, rate=rate)
-
-
-
 # 오디오 저장
 from scipy.io.wavfile import write
 
 text = """
-Smmoth like butter Like a criminal undercover
+Smooth like butter Like a criminal undercover
 Gon' pop like trouble Breakin' into your heart like that Cool shade stunner
 Yeah I owe it all to my mother Hot like summer.
+I I I did it!
 """
 
 utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tts_utils')
@@ -57,3 +38,4 @@ audio_numpy = audio[0].data.cpu().numpy()
 rate = 22050
 
 write("audio.wav", rate, audio_numpy)
+Audio(audio_numpy, rate=rate)
